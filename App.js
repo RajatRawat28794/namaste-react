d27ff38c1fa648af75d0f310e8510794908b6595
Rajat Rawat <rajatrapjput28794@gmail.com>
2024-02-11 07:17:51 -0800

    Changes 11/2/2024

--- App.js
+++ App.js
@@ -26,19 +26,76 @@
  * </div>
  * 
  */
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement(
-        "div", 
-        { id: "child" },
-        React.createElement("h1", {}, "I am h1 tag")
-    )
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//         "div", 
+//         { id: "child" },
+//         React.createElement("h1", {}, "I am h1 tag")
+//     )
+// );
+
+// console.log(parent);
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//    root.render(parent);
+
+ 
+// Lets say if we have siblings in div id=child
+
+{/* <div id="parent">
+ * <div id="child">
+ * <h1>I am h1 tag</h1>
+ * <h2>I am h2 tag</h2>
+ * </div>
+ * </div> */}
+
+//  const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//         "div", 
+//         { id: "child" },
+//         //we will put square brackets at line no 60.
+//        [React.createElement("h1", {}, "I am h1 tag"),
+//        React.createElement("h2", {}, "I am h2 tag")
+//        ]
+//     )
+// );
+
+// console.log(parent);
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//    root.render(parent);
+
+// 2nd Case:
+//----------------
+
+//  <div id="parent">
+//   <div id="child1">
+//   <h1>I am h1 tag</h1>
+//   <h2>I am h2 tag</h2>
+//   </div>
+//   <div id="child2">
+//   <h1>I am h1 tag</h1>
+//   <h2>I am h2 tag</h2>
+//   </div>
+//  </div> 
+
+const parent = React.createElement("div",{ id: "parent" },
+    React.createElement("div", { id: "child1" },
+        //we will put square brackets at line no 60.
+       [React.createElement("h1", {}, "I am h1 tag"),
+       React.createElement("h2", {}, "I am h2 tag")
+       ]),
+       React.createElement("div", { id: "child2" },
+       //we will put square brackets at line no 60.
+      [React.createElement("h1", {}, "I am h1 tag"),
+      React.createElement("h2", {}, "I am h2 tag")
+      ]),   
 );
+
+//JSX
 
 console.log(parent);
   const root = ReactDOM.createRoot(document.getElementById("root"));
    root.render(parent);
-
-
-
